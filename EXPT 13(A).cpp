f898e21e36915dc7f57265bddfc32b73f039825b
Deepak Singh Bhainsora <32154563+deepaksbmj@users.noreply.github.com>
2018-10-25 22:02:57 +0530

    Add files via upload

--- EXPT 13(A).cpp
+++ EXPT 13(A).cpp
@@ -3,32 +3,35 @@ using namespace std;
 int cost[10][10],m,i,j,k,n,stk[10],top,v,visit[10],visited[10];
 int main()
 {
-cout<<"Enter number of Vertices\n";
-cin>>n;
-cout<<"Enter number of Edges\n";
-cin>>m;
-cout<<"\nEdges\n";
-for(k=1;k<=m;k++)
+    cout<<"Enter number of Vertices\n";
+    cin>>n;
+    cout<<"Enter number of Edges\n";
+    cin>>m;
+    cout<<"\nEdges\n";
+    for(k=1;k<=m;k++)
     {
-cin>>i>>j;
-cost[i][j]=1;
-}
-cout<<"Enter initial Vertex\n";
-cin>>v;
-cout<<"Visited Vertex\n";
-cout<<v<<" ";
-visited[v]=1;
-k=1;
-while(k<n){
-    for(j=1;j<=n;j++){
-        if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1){
-            visit[j]=1;
-            stk[top++]=j;
-        }
+        cin>>i>>j;
+        cost[i][j]=1;
     }
-    v=stk[--top];
+    cout<<"Enter initial Vertex\n";
+    cin>>v;
+    cout<<"Visited Vertex\n";
     cout<<v<<" ";
-    k++;
-}
-return 0;
+    visited[v]=1;
+    k=1;
+    while(k<n)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1)
+            {
+                visit[j]=1;
+                stk[top++]=j;
+            }
+        }
+        v=stk[--top];
+        cout<<v<<" ";
+        k++;
+    }
+    return 0;
 }
--- EXPT 13(B).cpp
+++ EXPT 13(B).cpp
@@ -3,32 +3,35 @@ using namespace std;
 int cost[10][10],m,i,j,k,n,qu[10],front,rear,v,visit[10],visited[10];
 int main()
 {
-cout<<"Enter number of Vertices\n";
-cin>>n;
-cout<<"Enter number of Edges\n";
-cin>>m;
-cout<<"\nEdges\n";
-for(k=1;k<=m;k++)
+    cout<<"Enter number of Vertices\n";
+    cin>>n;
+    cout<<"Enter number of Edges\n";
+    cin>>m;
+    cout<<"\nEdges\n";
+    for(k=1;k<=m;k++)
     {
-cin>>i>>j;
-cost[i][j]=1;
-}
-cout<<"Enter initial Vertex\n";
-cin>>v;
-cout<<"Visited Vertex\n";
-cout<<v<<" ";
-visited[v]=1;
-k=1;
-while(k<n){
-    for(j=1;j<=n;j++){
-        if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1){
-            visit[j]=1;
-            qu[rear++]=j;
-        }
+        cin>>i>>j;
+        cost[i][j]=1;
     }
-    v=qu[front++];
+    cout<<"Enter initial Vertex\n";
+    cin>>v;
+    cout<<"Visited Vertex\n";
     cout<<v<<" ";
-    k++;
-}
-return 0;
+    visited[v]=1;
+    k=1;
+    while(k<n)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1)
+            {
+                visit[j]=1;
+                qu[rear++]=j;
+            }
+        }
+        v=qu[front++];
+        cout<<v<<" ";
+        k++;
+    }
+    return 0;
 }
